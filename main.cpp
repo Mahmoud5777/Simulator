c27be2390c94cf36acf49fad1c30f5f54a79c221
Mahmoud5777 <Mahmoud.ayedi@enis.tn>
2025-07-15 13:24:07 +0100

    my first commit after fixing the git !

--- main.cpp
+++ main.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is a simple simulator program." << std::endl;
+    // You can add more functionality here as needed.
     return 0;
 }
