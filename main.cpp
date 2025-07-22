b519818e6f99315f0859cb1197c86258e278d639
Mahmoud5777 <Mahmoud.ayedi@enis.tn>
2025-07-22 14:20:57 +0100

    pre exe

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 # include <iostream>
 # include "SimillatorController.hpp"
+# include "BusManager.hpp"
 int main() {
+    // Initialisation du BusManager
+    BusManager busManager;
+    if (!busManager.init()) {
+        std::cerr << "Failed to initialize BusManager." << std::endl;
+        return 1;
+    }
     SimillatorController simulator;
     simulator.run();
     
